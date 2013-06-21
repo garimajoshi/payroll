a43db39009880ed94edefd4d40abbdc4f7882c99
Garima Joshi <garimajoshi3@yahoo.in>
2013-06-21 18:41:33 +0530

    updated salary

--- fuel/app/classes/controller/salaries.php
+++ fuel/app/classes/controller/salaries.php
@@ -20,16 +20,34 @@ class Controller_Salaries extends Controller_Base {
         $this->template->content = View::forge('salaries/view', $data);
     }
 
+    public function action_archive($id = null) {
+        is_null($id) and Response::redirect('salaries');
+
+        if ($employee = Model_Employee::find($id)) {
+            $employee->lock = "true";
+
+            if ($employee->save()) {
+                Session::set_flash('success', 'Archived salary statement');
+            } else {
+                Session::set_flash('error', 'Could not archive');
+            }
+        } else {
+            Session::set_flash('error', 'Could not find salary statement #' . $id);
+        }
+
+        Response::redirect('salaries');
+    }
+
     public function action_create() {
-        
+
         if (Input::method() == 'POST') {
             $val = Model_Salary::validate('create');
-            
+
             if ($val->run()) {
-                
+
                 $var_gross = Input::post('gross');
                 $var_sdxo = Input::post('sdxo');
-                
+
                 $var_bonus1 = Input::post('bonus1');
                 $var_bonus2 = Input::post('bonus2');
                 $var_allowance1 = Input::post('allowance1');
@@ -41,39 +59,39 @@ class Controller_Salaries extends Controller_Base {
                 $var_deduction2 = Input::post('deduction2');
                 $var_deduction3 = Input::post('deduction3');
                 $var_leave = Input::post('leave');
-                
+
                 $c_basic = Model_Constant::find('first', array('where' => array('name' => 'basic')));
                 $var_basic_frac = $c_basic->value;
-                
+
                 $c_hra = Model_Constant::find('first', array('where' => array('name' => 'hra')));
                 $var_hra_frac = $c_hra->value;
-                
+
                 $c_pf_adjust = Model_Constant::find('first', array('where' => array('name' => 'pf_adjust')));
                 $var_pf_adjust_frac = $c_pf_adjust->value;
-                
+
                 $c_lta = Model_Constant::find('first', array('where' => array('name' => 'lta')));
                 $var_lta = $c_lta->value;
-                
+
                 $c_pf = Model_Constant::find('first', array('where' => array('name' => 'pf')));
                 $pf = $c_pf->value;
-                
+
                 $c_medical = Model_Constant::find('first', array('where' => array('name' => 'medical')));
                 $var_medical = $c_medical->value;
-                
+
                 $c_travel = Model_Constant::find('first', array('where' => array('name' => 'travel')));
                 $var_travel = $c_travel->value;
-                
+
                 $var_adj_sdxo = $var_gross - $var_sdxo;
-                $var_pf_adjust = (Input::post('pf_applicable') == "1") ? ($var_adj_sdxo / $var_pf_adjust_frac) : $var_adj_sdxo;        
+                $var_pf_adjust = (Input::post('pf_applicable') == "1") ? ($var_adj_sdxo / $var_pf_adjust_frac) : $var_adj_sdxo;
                 $var_basic = $var_basic_frac * $var_pf_adjust;
                 $var_hra = $var_hra_frac * $var_pf_adjust;
                 $var_pf_value = (Input::post('pf_applicable') == "1") ? ($pf * $var_basic) : 0;
-                
+
                 $var_credit_other = $var_pf_adjust - ($var_basic + $var_hra + $var_lta + $var_medical + $var_travel + $var_pf_value);
-                $var_credit_total = $var_basic + $var_hra +$var_lta + $var_medical + $var_travel + $var_pf_value + $var_credit_other + $var_leave + $var_bonus1 + $var_bonus2 + $var_allowance1 + $var_allowance2 + $var_allowance3;
+                $var_credit_total = $var_basic + $var_hra + $var_lta + $var_medical + $var_travel + $var_pf_value + $var_credit_other + $var_leave + $var_bonus1 + $var_bonus2 + $var_allowance1 + $var_allowance2 + $var_allowance3;
                 $var_total_debit = $var_professional_tax + $var_income_tax + $var_pf_value + $var_deduction1 + $var_deduction2 + $var_deduction3;
                 $var_net = $var_credit_total - $var_total_debit;
-                
+
                 $salary = Model_Salary::forge(array(
                             'month' => Input::post('month'),
                             'year' => Input::post('year'),
@@ -132,28 +150,82 @@ class Controller_Salaries extends Controller_Base {
         $val = Model_Salary::validate('edit');
 
         if ($val->run()) {
+
+            $var_gross = Input::post('gross');
+            $var_sdxo = Input::post('sdxo');
+
+            $var_bonus1 = Input::post('bonus1');
+            $var_bonus2 = Input::post('bonus2');
+            $var_allowance1 = Input::post('allowance1');
+            $var_allowance2 = Input::post('allowance2');
+            $var_allowance3 = Input::post('allowance3');
+            $var_income_tax = Input::post('income_tax');
+            $var_professional_tax = Input::post('professional_tax');
+            $var_deduction1 = Input::post('deduction1');
+            $var_deduction2 = Input::post('deduction2');
+            $var_deduction3 = Input::post('deduction3');
+            $var_leave = Input::post('leave');
+
+            $c_basic = Model_Constant::find('first', array('where' => array('name' => 'basic')));
+            $var_basic_frac = $c_basic->value;
+
+            $c_hra = Model_Constant::find('first', array('where' => array('name' => 'hra')));
+            $var_hra_frac = $c_hra->value;
+
+            $c_pf_adjust = Model_Constant::find('first', array('where' => array('name' => 'pf_adjust')));
+            $var_pf_adjust_frac = $c_pf_adjust->value;
+
+            $c_lta = Model_Constant::find('first', array('where' => array('name' => 'lta')));
+            $var_lta = $c_lta->value;
+
+            $c_pf = Model_Constant::find('first', array('where' => array('name' => 'pf')));
+            $pf = $c_pf->value;
+
+            $c_medical = Model_Constant::find('first', array('where' => array('name' => 'medical')));
+            $var_medical = $c_medical->value;
+
+            $c_travel = Model_Constant::find('first', array('where' => array('name' => 'travel')));
+            $var_travel = $c_travel->value;
+
+            $var_adj_sdxo = $var_gross - $var_sdxo;
+            $var_pf_adjust = (Input::post('pf_applicable') == "1") ? ($var_adj_sdxo / $var_pf_adjust_frac) : $var_adj_sdxo;
+            $var_basic = $var_basic_frac * $var_pf_adjust;
+            $var_hra = $var_hra_frac * $var_pf_adjust;
+            $var_pf_value = (Input::post('pf_applicable') == "1") ? ($pf * $var_basic) : 0;
+
+            $var_credit_other = $var_pf_adjust - ($var_basic + $var_hra + $var_lta + $var_medical + $var_travel + $var_pf_value);
+            $var_credit_total = $var_basic + $var_hra + $var_lta + $var_medical + $var_travel + $var_pf_value + $var_credit_other + $var_leave + $var_bonus1 + $var_bonus2 + $var_allowance1 + $var_allowance2 + $var_allowance3;
+            $var_total_debit = $var_professional_tax + $var_income_tax + $var_pf_value + $var_deduction1 + $var_deduction2 + $var_deduction3;
+            $var_net = $var_credit_total - $var_total_debit;
+
             $salary->month = Input::post('month');
             $salary->year = Input::post('year');
+            $salary->lock = 'false';
             $salary->pf_applicable = Input::post('pf_applicable');
-            $salary->gross = Input::post('gross');
-            $salary->sdxo = Input::post('sdxo');
-            $salary->pf_adjust = Input::post('pf_adjust');
-            $salary->basic = Input::post('basic');
-            $salary->hra = Input::post('hra');
-            $salary->lta = Input::post('lta');
-            $salary->medical = Input::post('medical');
-            $salary->travel = Input::post('travel');
-            $salary->pf_value = Input::post('pf_value');
-            $salary->credit_other = Input::post('credit_other');
-            $salary->bonus1 = Input::post('bonus1');
-            $salary->bonus2 = Input::post('bonus2');
-            $salary->bonus3 = Input::post('bonus3');
-            $salary->leave = Input::post('leave');
-            $salary->misc1 = Input::post('misc1');
-            $salary->misc2 = Input::post('misc2');
-            $salary->misc3 = Input::post('misc3');
-            $salary->income_tax = Input::post('income_tax');
-            $salary->net = Input::post('net');
+            $salary->gross = $var_gross;
+            $salary->sdxo = $var_sdxo;
+            $salary->pf_adjust = $var_pf_adjust;
+            $salary->basic = $var_basic;
+            $salary->hra = $var_hra;
+            $salary->lta = $var_lta;
+            $salary->medical = $var_medical;
+            $salary->travel = $var_travel;
+            $salary->pf_value = $var_pf_value;
+            $salary->credit_other = $var_credit_other;
+            $salary->bonus1 = $var_bonus1;
+            $salary->bonus2 = $var_bonus2;
+            $salary->allowance1 = $var_allowance1;
+            $salary->leave = $var_leave;
+            $salary->allowance2 = $var_allowance2;
+            $salary->allowance3 = $var_allowance3;
+            $salary->credit_total = $var_credit_total;
+            $salary->income_tax = $var_income_tax;
+            $salary->professional_tax = $var_professional_tax;
+            $salary->deduction1 = $var_deduction1;
+            $salary->deduction2 = $var_deduction2;
+            $salary->deduction3 = $var_deduction3;
+            $salary->total_debit = $var_total_debit;
+            $salary->net = $var_net;
 
             if ($salary->save()) {
                 Session::set_flash('success', 'Updated salary #' . $id);
@@ -164,31 +236,83 @@ class Controller_Salaries extends Controller_Base {
             }
         } else {
             if (Input::method() == 'POST') {
+
+                $var_gross = Input::post('gross');
+                $var_sdxo = Input::post('sdxo');
+
+                $var_bonus1 = Input::post('bonus1');
+                $var_bonus2 = Input::post('bonus2');
+                $var_allowance1 = Input::post('allowance1');
+                $var_allowance2 = Input::post('allowance2');
+                $var_allowance3 = Input::post('allowance3');
+                $var_income_tax = Input::post('income_tax');
+                $var_professional_tax = Input::post('professional_tax');
+                $var_deduction1 = Input::post('deduction1');
+                $var_deduction2 = Input::post('deduction2');
+                $var_deduction3 = Input::post('deduction3');
+                $var_leave = Input::post('leave');
+
+                $c_basic = Model_Constant::find('first', array('where' => array('name' => 'basic')));
+                $var_basic_frac = $c_basic->value;
+
+                $c_hra = Model_Constant::find('first', array('where' => array('name' => 'hra')));
+                $var_hra_frac = $c_hra->value;
+
+                $c_pf_adjust = Model_Constant::find('first', array('where' => array('name' => 'pf_adjust')));
+                $var_pf_adjust_frac = $c_pf_adjust->value;
+
+                $c_lta = Model_Constant::find('first', array('where' => array('name' => 'lta')));
+                $var_lta = $c_lta->value;
+
+                $c_pf = Model_Constant::find('first', array('where' => array('name' => 'pf')));
+                $pf = $c_pf->value;
+
+                $c_medical = Model_Constant::find('first', array('where' => array('name' => 'medical')));
+                $var_medical = $c_medical->value;
+
+                $c_travel = Model_Constant::find('first', array('where' => array('name' => 'travel')));
+                $var_travel = $c_travel->value;
+
+                $var_adj_sdxo = $var_gross - $var_sdxo;
+                $var_pf_adjust = (Input::post('pf_applicable') == "1") ? ($var_adj_sdxo / $var_pf_adjust_frac) : $var_adj_sdxo;
+                $var_basic = $var_basic_frac * $var_pf_adjust;
+                $var_hra = $var_hra_frac * $var_pf_adjust;
+                $var_pf_value = (Input::post('pf_applicable') == "1") ? ($pf * $var_basic) : 0;
+
+                $var_credit_other = $var_pf_adjust - ($var_basic + $var_hra + $var_lta + $var_medical + $var_travel + $var_pf_value);
+                $var_credit_total = $var_basic + $var_hra + $var_lta + $var_medical + $var_travel + $var_pf_value + $var_credit_other + $var_leave + $var_bonus1 + $var_bonus2 + $var_allowance1 + $var_allowance2 + $var_allowance3;
+                $var_total_debit = $var_professional_tax + $var_income_tax + $var_pf_value + $var_deduction1 + $var_deduction2 + $var_deduction3;
+                $var_net = $var_credit_total - $var_total_debit;
+
+
                 $salary->month = $val->validated('month');
                 $salary->year = $val->validated('year');
+                $salary->lock = 'false';
                 $salary->pf_applicable = $val->validated('pf_applicable');
-                $salary->pf_date = $val->validated('pf_date');
-                $salary->pf_scheme = $val->validated('pf_scheme');
-                $salary->pf_number = $val->validated('pf_number');
-                $salary->gross = $val->validated('gross');
-                $salary->sdxo = $val->validated('sdxo');
-                $salary->pf_adjust = $val->validated('pf_adjust');
-                $salary->basic = $val->validated('basic');
-                $salary->hra = $val->validated('hra');
-                $salary->lta = $val->validated('lta');
-                $salary->medical = $val->validated('medical');
-                $salary->travel = $val->validated('travel');
-                $salary->pf_value = $val->validated('pf_value');
-                $salary->credit_other = $val->validated('credit_other');
-                $salary->bonus1 = $val->validated('bonus1');
-                $salary->bonus2 = $val->validated('bonus2');
-                $salary->bonus3 = $val->validated('bonus3');
-                $salary->leave = $val->validated('leave');
-                $salary->misc1 = $val->validated('misc1');
-                $salary->misc2 = $val->validated('misc2');
-                $salary->misc3 = $val->validated('misc3');
-                $salary->income_tax = $val->validated('income_tax');
-                $salary->net = $val->validated('net');
+                $salary->gross = $var_gross;
+                $salary->sdxo = $var_sdxo;
+                $salary->pf_adjust = $var_pf_adjust;
+                $salary->basic = $var_basic;
+                $salary->hra = $var_hra;
+                $salary->lta = $var_lta;
+                $salary->medical = $var_medical;
+                $salary->travel = $var_travel;
+                $salary->pf_value = $var_pf_value;
+                $salary->credit_other = $var_credit_other;
+                $salary->bonus1 = $var_bonus1;
+                $salary->bonus2 = $var_bonus2;
+                $salary->allowance1 = $var_allowance1;
+                $salary->leave = $var_leave;
+                $salary->allowance2 = $var_allowance2;
+                $salary->allowance3 = $var_allowance3;
+                $salary->credit_total = $var_credit_total;
+                $salary->income_tax = $var_income_tax;
+                $salary->professional_tax = $var_professional_tax;
+                $salary->deduction1 = $var_deduction1;
+                $salary->deduction2 = $var_deduction2;
+                $salary->deduction3 = $var_deduction3;
+                $salary->total_debit = $var_total_debit;
+                $salary->net = $var_net;
 
                 Session::set_flash('error', $val->error());
             }
