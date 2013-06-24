86754fee1991499f6f0c6dcbadcede029a16169a
Garima Joshi <garimajoshi3@yahoo.in>
2013-06-24 11:32:56 +0530

    few changes to fix employee

--- fuel/app/views/leaves/view.php
+++ fuel/app/views/leaves/view.php
@@ -1,17 +1,17 @@
-<h2>Viewing <span class='muted'>#<?php echo $leafe->id; ?></span></h2>
+<h2>Viewing <span class='muted'>#<?php echo $leave->id; ?></span></h2>
 
 <p>
 	<strong>Employee id:</strong>
-	<?php echo $leafe->employee_id; ?></p>
+	<?php echo $leave->employee_id; ?></p>
 <p>
 	<strong>Date of leave:</strong>
-	<?php echo $leafe->date_of_leave; ?></p>
+	<?php echo $leave->date_of_leave; ?></p>
 <p>
 	<strong>Reason:</strong>
-	<?php echo $leafe->reason; ?></p>
+	<?php echo $leave->reason; ?></p>
 <p>
 	<strong>Type:</strong>
-	<?php echo $leafe->type; ?></p>
+	<?php echo $leave->type; ?></p>
 
-<?php echo Html::anchor('leaves/edit/'.$leafe->id, 'Edit'); ?> |
+<?php echo Html::anchor('leaves/edit/'.$leave->id, 'Edit'); ?> |
 <?php echo Html::anchor('leaves', 'Back'); ?>
